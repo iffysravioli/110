c3597fa7961890cc155b2dd4bd47a2ea9ff7902b
LesterHG12 <92682812+LesterHG12@users.noreply.github.com>
2023-09-17 00:33:13 -0400

    Calendar

--- wellnesswave/src/App.js
+++ wellnesswave/src/App.js
@@ -1,13 +1,14 @@
 import "bootstrap/dist/css/bootstrap.min.css"
 import "./App.css"
 import { BrowserRouter, Routes, Route } from "react-router-dom"
+import Home from "./Home"
 import Auth from "./Auth"
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Auth />} />
+        <Route path="/" element={<Home />} />
       </Routes>
     </BrowserRouter>
   )
