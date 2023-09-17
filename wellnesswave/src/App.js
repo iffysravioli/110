7d60b6d7e43f2a25293e8c249add9f0ebe4da3ba
LesterHG12 <92682812+LesterHG12@users.noreply.github.com>
2023-09-16 22:23:53 -0400

    Set

--- wellnesswave/src/App.js
+++ wellnesswave/src/App.js
@@ -1,5 +1,4 @@
 import "bootstrap/dist/css/bootstrap.min.css"
-import logo from './logoww.png';
 import "./App.css"
 import { BrowserRouter, Routes, Route } from "react-router-dom"
 import Auth from "./Auth"
